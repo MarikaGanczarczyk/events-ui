50c275e80777bdb386ccb59cf2735a2e19d39f90
marik1504894 <marik1504894@Mastek.com>
2026-08-19 12:46:05 +0100

    Added search bar and 2x sort

--- src/EventsPage.js
+++ src/EventsPage.js
@@ -2,9 +2,24 @@ import React, { useEffect, useState } from 'react'
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
-
+import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
+        
 const EventsPage = () => {
 const [events, setEvents] =useState("")
+
+const [selectedStatus, setSelectedStatus] = useState(null);
+const [selectedCriticality, setSelectedCriticality] = useState(null);
+
+const statusOptions = [
+{ label: "Active", value: true },
+{ label: "Non Active", value: false }
+];
+
+const criticalityOptions = [
+{ label: "Critical", value: true },
+{ label: "Non Critical", value: false }
+];
 
 
 useEffect(()=>{
@@ -27,6 +42,36 @@ return (
         <h2>Events Dashboard</h2>
         <p className="page-subtitle">Search and manage events</p>
       </div>
+      
+      <div className='search-container'>
+
+     <span className="p-input-icon-left">
+    <i className="pi pi-search" />
+    <InputText placeholder="Search even type" /></span>
+
+<Dropdown
+value={selectedStatus}
+onChange={(e) => setSelectedStatus(e.value)}
+options={statusOptions}
+optionLabel="label"
+placeholder="Status"
+className="dropdown-filter"
+/>
+
+<Dropdown
+value={selectedCriticality}
+onChange={(e) => setSelectedCriticality(e.value)}
+options={criticalityOptions}
+optionLabel="label"
+placeholder="Criticality"
+className="dropdown-filter"
+/> 
+
+<Button
+label="+ New Event"
+className="new-event-btn"/>
+      </div>
+
 
 <div className='table-container'>
 <DataTable value={events}>
