4e70e0b9f87d07835dd5fe508903adec94b72efb
marik1504894 <marik1504894@Mastek.com>
2026-08-20 15:21:41 +0100

    Created pop up for CreateEvent

--- src/CreateEvent.js
+++ src/CreateEvent.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import { useState } from "react";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+
 
 const CreateEvent = () => {
+const [visible, setVisible] = useState(false);
+const [eventType, setEventType] = useState("");
+const [description, setDescription] = useState("");
+
+
+
+
   return (
-    <div>CreateEvent</div>
-  )
-}
+    <>
+   <Button
+   label="+ New Event"
+   className="new-event-btn" onClick={()=> setVisible(true)}/>
+   <Dialog header="Create New Event" visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)}>
+              <div className="form-field">
+   
+             <label htmlFor="eventName"></label>
+             <InputText
+               id="eventtype"
+               value={eventType}
+               onChange={(e) => setEventType(e.target.value)}
+               placeholder="Enter event type"
+             />
+           </div>
+   
+           <div className="form-field">
+             <label htmlFor="description"></label>
+             <InputText
+               id="description"
+               value={description }
+               onChange={(e) => setDescription(e.target.value)}
+               placeholder="Enter description"
+             />
+           </div>
+         
+   
+           <div className="form-actions">
+             <Button
+               label="Cancel"
+               severity="secondary"
+               onClick={() => setVisible(false)}
+             /> 
+   
+             {/* <Button
+               label="Create Event"
+               icon="pi pi-check"
+               onClick={handleCreateEvent}
+             /> */}
+           </div>
+           </Dialog>
+           </>
+          
+  )}
 
 export default CreateEvent
--- src/EventsPage.js
+++ src/EventsPage.js
@@ -1,94 +1,94 @@
-import React, { useEffect, useState } from 'react'
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { Button } from 'primereact/button';
-import { Dropdown } from 'primereact/dropdown';
-import { InputText } from 'primereact/inputtext';
-        
+import React, { useEffect, useState } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Button } from "primereact/button";
+import { Dropdown } from "primereact/dropdown";
+import { InputText } from "primereact/inputtext";
+
+import CreateEvent from "./CreateEvent";
+
 const EventsPage = () => {
-const [events, setEvents] =useState([])
+  const [events, setEvents] = useState([]);
 
-const [selectedStatus, setSelectedStatus] = useState(null);
-const [selectedCriticality, setSelectedCriticality] = useState(null);
+  const [selectedStatus, setSelectedStatus] = useState(null);
+  const [selectedCriticality, setSelectedCriticality] = useState(null);
 
-const statusOptions = [
-{ label: "Active", value: true },
-{ label: "Non Active", value: false }
-];
+  
 
-const criticalityOptions = [
-{ label: "Critical", value: true },
-{ label: "Non Critical", value: false }
-];
+  const statusOptions = [
+    { label: "Active", value: true },
+    { label: "Non Active", value: false },
+  ];
+  const criticalityOptions = [
+    { label: "Critical", value: true },
+    { label: "Non Critical", value: false },
+  ];
 
+  useEffect(() => {
+    fetch("http://localhost:3001/events")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setEvents(data);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
 
-useEffect(()=>{
-fetch('http://localhost:3001/events')
-.then((res)=>res.json())
-.then((data)=>setEvents(data))
-.catch((err)=>console.log(err.message))
-},[])
-
-const actionBodyTemplate = () => {
-return (
-<Button icon="pi pi-ellipsis-v" className='action-btn'
-/>
-);
-};
+  const actionBodyTemplate = () => {
+    return <Button icon="pi pi-ellipsis-v" className="action-btn" />;
+  };
 
   return (
-    <div className='container'>
- <div className="page-header">
+    <div className="container">
+      <div className="page-header">
         <h2>Events </h2>
         <p className="page-subtitle">Search and manage events</p>
       </div>
-      
-      <div className='search-container'>
 
-     <span className="p-input-icon-left">
-    <i className="pi pi-search" />
-    <InputText placeholder="Search even type" /></span>
+      <div className="search-container">
+        <span className="p-input-icon-left">
+          <i className="pi pi-search" />
+          <InputText placeholder="Search even type" />
+        </span>
 
-<Dropdown
-value={selectedStatus}
-onChange={(e) => setSelectedStatus(e.value)}
-options={statusOptions}
-optionLabel="label"
-placeholder="Status"
-className="dropdown-filter"
-/>
+        <Dropdown
+          value={selectedStatus}
+          onChange={(e) => setSelectedStatus(e.value)}
+          options={statusOptions}
+          optionLabel="label"
+          placeholder="Status"
+          className="dropdown-filter"
+        />
 
-<Dropdown
-value={selectedCriticality}
-onChange={(e) => setSelectedCriticality(e.value)}
-options={criticalityOptions}
-optionLabel="label"
-placeholder="Criticality"
-className="dropdown-filter"
-/> 
-
-<Button
-label="+ New Event"
-className="new-event-btn"/>
+        <Dropdown
+          value={selectedCriticality}
+          onChange={(e) => setSelectedCriticality(e.value)}
+          options={criticalityOptions}
+          optionLabel="label"
+          placeholder="Criticality"
+          className="dropdown-filter"
+        />
+        <div>
+          <div>
+            <CreateEvent />
+          </div>
+        </div>
       </div>
 
-
-<div className='table-container'>
-<DataTable value={events}>
-<Column field="eventtype" header="Event Type"/>
-<Column field="eventdescription" header="Description"/>
-<Column field="eventowner" header="Owner"/>
-<Column field="ipfstage" header="Stage"/>
-<Column field="isactive" header="Active" />
-<Column field="criticalevent" header="Critical" />
-<Column field="isreusable" header="Reusable" />
-<Column header="Actions" body={actionBodyTemplate} />
-</DataTable>
-
-</div>
-
+      <div className="table-container">
+        <DataTable value={events}>
+          <Column field="eventtype" header="Event Type" />
+          <Column field="eventdescription" header="Description" />
+          <Column field="eventowner" header="Owner" />
+          <Column field="ipfstage" header="Stage" />
+          <Column field="isactive" header="Active" />
+          <Column field="criticalevent" header="Critical" />
+          <Column field="isreusable" header="Reusable" />
+          <Column header="Actions" body={actionBodyTemplate} />
+        </DataTable>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EventsPage
+export default EventsPage;
